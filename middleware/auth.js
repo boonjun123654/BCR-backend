0e2ae74b1186160a760489afaa08d673f5da29a2
boonjun123654 <boonjunlee@gmail.com>
2026-02-08 23:26:50 +0800

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,21 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function(req, res, next){
-  const auth = req.headers.authorization;
-  if(!auth) return res.status(401).json({ msg: "No token" });
+  const authHeader = req.headers.authorization;
 
-  const token = auth.split(" ")[1];
+  if(!authHeader || !authHeader.startsWith("Bearer ")){
+    return res.status(401).json({ msg: "No token" });
+  }
+
+  const token = authHeader.slice(7); // 去掉 "Bearer "
+
   try{
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   }catch(err){
-    res.status(401).json({ msg: "Invalid token" });
+  if(err.name === "TokenExpiredError"){
+    return res.status(401).json({ msg: "Token expired" });
   }
-};
+  return res.status(401).json({ msg: "Invalid token" });
+}
