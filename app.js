cdddff21d9166f04a0663e9dde4233427b33ba8f
boonjun123654 <boonjunlee@gmail.com>
2026-02-08 23:04:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", authRoutes);
+
+app.listen(3000, () => {
+  console.log("Auth Service running on http://localhost:3000");
+});
 
