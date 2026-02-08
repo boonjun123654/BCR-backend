fcc5191a2c50a656ea63efffaebc7872c386d131
boonjun123654 <boonjunlee@gmail.com>
2026-02-08 23:03:27 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,11 @@
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "casino",
+});
+
+module.exports = pool;
 
